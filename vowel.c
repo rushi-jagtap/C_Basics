e268b444bcfb9a0defecb366f20602e79ee2b1ea
rushi-jagtap <jagtaprushikesh512@gmail.com>
2021-09-07 21:13:08 +0530

    init

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()lowercase_vowel(c == 'a' || c == e || c == i || c == o || c == u)
+{
+    char c;
+    printf("\n Enetr character");
+    scanf(" %c", &c);
+    
+    if((c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') || (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'))
+    { 
+        printf("\n Vowel");
+    }
+    else
+    {
+        printf("\n Consonant");
+    }
+    return 0;
+}
